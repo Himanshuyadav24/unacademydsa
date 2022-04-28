1be8ddf3a68300ca3f7550b56e9dd53bb687b253
Himanshu <Himanshu1156044@gmail.com>
2022-04-28 19:53:12 +0530

    love

--- cyclicrotate.cpp
+++ cyclicrotate.cpp
@@ -1,33 +1,25 @@
 #include<iostream>
 using namespace std;
-
-void printArray(int arr[], int size)
+void printArray(int array[],int size)
 {
     for (int i = 0; i < size; i++)
     {
-        cout<<arr[i]<<" ";
+        cout<<array[i]<<" ";
     }
-    
 }
-void arrayRotate(int arr[],int size)
+void cyclicRotate(int array[],int size)
 {
-    int i = 0;
-    int temp;
-    while (i+1 <= size)
-    {   
-        temp = arr[i+1];
-        arr[i+1]=arr[i];
-        i++;
-        arr[i+1]= temp;
-
-    }
+   int x = array[size-1];
+   for (int i = size-1; i > 0; i--)
+   {
+       array[i]=array[i-1];
+   }
+   array[0] = x; 
 }
 int main()
 {
-    int arr[5] = {0,1,2,3,4};
+    int array[5] = {0,1,2,3,4};
 
-    arrayRotate(arr,5);
-    printArray(arr,5);
-
-    return 0;
+    cyclicRotate(array,5);
+    printArray(array,5);
 }
