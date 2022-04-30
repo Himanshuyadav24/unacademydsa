6d435dabf607e920dcac01e20eeea4e43278703c
Himanshu <Himanshu1156044@gmail.com>
2022-04-30 21:02:04 +0530

    love

--- Kaden's Algo.cpp
+++ Kaden's Algo.cpp
@@ -1,12 +1,22 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int arr[8] = {-2,-3,4,-1,-2,1,5,-3};
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[8] = {-2, -3, 4, -1, -2, 1, 5, -3};
 
+//     int sum = 0;
+//     long long ans = INT_MIN;
+
+//     for (int i = 0; i < 8; i++)
+//     {
+//         sum = sum + arr[i];
+
+//         ans = max(sum, ans);
+
+//         if (sum < 0)
+//             sum = 0;
+//     }
     
 
-
-
-    return 0;
-}
+//     return 0;
+// }
