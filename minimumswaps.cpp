5d1cca17f1510cfc5e2c672bc0c05e5a9c80dca1
Himanshu <Himanshu1156044@gmail.com>
2022-05-01 19:25:48 +0530

    love

--- minimumswaps.cpp
+++ minimumswaps.cpp
@@ -1,31 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int minSwap(int arr[],int size,int k)
+int minSwap(int arr[], int size, int k)
 {
-    int count=0;
+    int count = 0;
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            if(arr[i]>=k && arr[j]<=k)
+            if (arr[i] >= k && arr[j] <= k)
             {
-                swap(arr[i],arr[j]);
+                swap(arr[i], arr[j]);
                 count++;
             }
         }
+        count--;
+        cout << count;
+        return count;
     }
-    count--;
-    cout<<count;
-    return count;
 }
 
-int main()
-{
-    int arr[8] = {232,4,1,65,76,3,0,42};
+    int main()
+    {
+        int arr[8] = {232, 4, 1, 65, 76, 3, 0, 42};
 
-    int k =5;
+        int k = 5;
 
-    minSwap(arr,8,k);
-    return 0;
-}
+        minSwap(arr, 8, k);
+
+        return 0;
+    }
--- wavematrix.cpp
+++ wavematrix.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int arr[4][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    int row = 4, col =4;
+    int arr[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    int row = 4, col = 4;
     int total = row * col;
 
-    
-
-
-
-
-
-
-
-
-
-
+    for (int i = 0; i < row; i++)
+    {
+        if (i%2 == 0)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            for (int j = col-1; j >= 0; j--)
+            {
+                cout<<arr[i][j]<<" ";
+            }
+            
+        }
+    }
 
     return 0;
 }
