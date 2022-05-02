9cc37c24ac054c578c3979927adffd2c168f1dc2
Himanshu <Himanshu1156044@gmail.com>
2022-05-02 22:37:02 +0530

    love

---
+++ problemoftheday.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    char s[] = "ab#c" ;
+    char t[] = "ad#c" ;
+
+    // # means backspace
+
+    
+   int size = sizeof(s)/sizeof(s[2]);
+   cout<<size<<endl;
+
+
+   
+
+
+
+
+    return 0;
+}
--- wavematrix.cpp
+++ wavematrix.cpp
@@ -21,7 +21,6 @@ int main()
             {
                 cout<<arr[i][j]<<" ";
             }
-            
         }
     }
 
