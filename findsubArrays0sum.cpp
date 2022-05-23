51c03f5602d1c678835cf9b445127af206469fa2
Himanshu <Himanshu1156044@gmail.com>
2022-05-23 23:37:07 +0530

    love

---
+++ findsubArrays0sum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<map>
+// #include <array>
+
+using namespace std;
+int main()
+
+{
+    int arr[] = {0, 0, 5, 5, 0, 0};
+    int preSum[] = {};
+    int n = sizeof(arr) / sizeof(int);
+    int sum =0;
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+    
+
+
+
+
+
+
+
+
+
+    cout << endl;
+    cout << count;
+    return 0;
+}
--- subarray0and1.cpp
+++ subarray0and1.cpp
@@ -1,37 +1,49 @@
-#include<iostream>
-#include<map>
+#include <iostream>
+#include <map>
 using namespace std;
 int main()
 {
-    int arr[] = {1,0,0,1,0,1,1};
+    int arr[] = {1, 0, 0, 1, 0, 1, 1};
 
     int n = 7;
 
-    int i = 0;
     int sum = 0;
+
     int count = 0;
-    map<int,int>map;
-    while(i<n)
+
+    map<int, int> map;
+
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i]==0)
+        if (arr[i] == 0)
         {
-            arr[i]= -1;
+            arr[i] = -1;
         }
 
-        sum +=arr[i];
+        sum += arr[i];
 
-        map[sum]++;
-
-        if(map[sum]==0)
+        if (sum == 0)
         {
             count++;
         }
-        i++;
 
+        if (map[sum])
+        {
+            count += map[sum];
+        }
 
+        if (map[sum] == 0)
+            map[sum] = 1;
+        else
+            map[sum]++;
+
+        for (auto it = map.cbegin(); it != map.cend(); ++it)
+        {
+            std::cout << "{" << (*it).first << ": " << (*it).second << "}\n";
+        }
     }
 
-    cout<<count;
+    cout << count;
 
     return 0;
 }
